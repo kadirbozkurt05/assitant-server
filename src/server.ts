928e79f238e852115b696800f19527a815ee7668
Kadir Bozkurt <kadirbozkurt05@yandex.com>
2024-12-08 13:17:46 +0100

    first commit

--- src/server.ts
+++ src/server.ts
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3000;
 connectDB();
 
 // Middleware for CORS
-const allowedOrigins = ['http://localhost:5173'];
+const allowedOrigins = ['http://localhost:5173','https://zp1v56uxy8rdx5ypatb0ockcb9tr6a-oci3--5173--fc837ba8.local-credentialless.webcontainer-api.io'];
 app.use(cors({
   origin: (origin, callback) => {
     // Allow requests with no origin (like mobile apps or curl requests)
